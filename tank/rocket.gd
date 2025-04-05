0bfaf23751190b0fad7c44ebfd226ad61c924c6f
feored <feordo@gmail.com>
2025-04-05 11:59:33 +0200

    base enemy

--- tank/rocket.gd
+++ tank/rocket.gd
@@ -1,5 +1,5 @@
 extends Area2D
-@export var speed = 100
+@export var speed = 150
 var ground: TileMapLayer
 
 const SIZE = Vector2(16, 24)
@@ -11,11 +11,19 @@ func _physics_process(delta: float) -> void:
 		print("Rocket hit: ", closest_cell)
 		ground.set_cell(closest_cell, Constants.EMPTY_DIRT_TILE_ID, Vector2i.ZERO)
 		print("new cell: ", ground.get_cell_source_id(closest_cell))
-		self.speed = self.speed * 0.9 - 1
+		self.speed = self.speed * 0.9 - 2
 	if speed < 20:
 		self.explode()
 
-func explode():
+func explode(center = self.position) -> void:
+	print("Rocket exploded")
+	var closest_cell: Vector2 = ground.local_to_map(center)
+	var neighbors = Utils.get_neighboring_cells(ground, closest_cell)
+	if (center != self.position):
+		ground.set_cell(closest_cell, Constants.EMPTY_DIRT_TILE_ID, Vector2i.ZERO)
+	for cell in neighbors:
+		if ground.get_cell_source_id(cell) in Constants.DIRT_TILE_IDS:
+			ground.set_cell(cell, Constants.EMPTY_DIRT_TILE_ID, Vector2i.ZERO)
 	self.queue_free()
 
 
@@ -29,5 +37,7 @@ func explode():
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemies"):
 		print("Rocket hit: ", area.name)
+		var shape = area.get_node("CollisionShape2D")
+		self.explode(area.position)
 		area.queue_free()
-		self.explode()
+		
