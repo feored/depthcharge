936f8bb766880d5e1f016034c133ee439a87edd5
feored <feordo@gmail.com>
2025-04-05 11:36:49 +0200

    basic enemy

--- constants.gd
+++ constants.gd
@@ -1,1 +1,4 @@
 extends Node
+
+const DIRT_TILE_IDS = [1, 2]
+const EMPTY_DIRT_TILE_ID = 0
--- tank/rocket.gd
+++ tank/rocket.gd
@@ -7,10 +7,16 @@ const SIZE = Vector2(16, 24)
 func _physics_process(delta: float) -> void:
 	self.position += Vector2(0, speed * delta)
 	var closest_cell: Vector2 = ground.local_to_map(self.position)
-	if ground.get_cell_source_id(closest_cell) != -1:
+	if ground.get_cell_source_id(closest_cell) in Constants.DIRT_TILE_IDS:
 		print("Rocket hit: ", closest_cell)
-		ground.erase_cell(closest_cell)
-		self.speed -= 5
+		ground.set_cell(closest_cell, Constants.EMPTY_DIRT_TILE_ID, Vector2i.ZERO)
+		print("new cell: ", ground.get_cell_source_id(closest_cell))
+		self.speed = self.speed * 0.9 - 1
+	if speed < 20:
+		self.explode()
+
+func explode():
+	self.queue_free()
 
 
 # func _on_body_entered(body: Node2D) -> void:
@@ -18,3 +24,10 @@ func _physics_process(delta: float) -> void:
 # 	if body.is_in_group("ground"):
 # 		var tilemap = body as TileMapLayer
 # 		tilemap.erase_cell(tilemap.local_to_map(self.position))
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemies"):
+		print("Rocket hit: ", area.name)
+		area.queue_free()
+		self.explode()
